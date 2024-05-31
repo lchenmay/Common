144b7518b721116f3d8de74cb74586b363a2a75d
RR <RR@MAIN>
2024-05-30 22:20:42 -0400

    s

---
+++ MauiFsLogics/Types.fs
@@ -0,0 +1,16 @@
+﻿module AioLogics.Types
+
+open System
+open System.Collections.Generic
+
+open Microsoft.Maui
+open Microsoft.Maui.Graphics
+open Microsoft.Maui.Controls
+open Microsoft.Maui.Layouts
+
+type Runtime = {
+mutable spa: ContentPage }
+
+let runtime = {
+    spa = new ContentPage() }
+
---
+++ UtilMaui/SPA.fs
@@ -0,0 +1,15 @@
+﻿module UtilMaui.SPA
+
+open System
+open System.Collections.Generic
+
+open Microsoft.Maui
+open Microsoft.Maui.Graphics
+open Microsoft.Maui.Controls
+open Microsoft.Maui.Layouts
+
+type SpaRuntime = {
+mutable page: ContentPage }
+
+let runtime = {
+    page = new ContentPage() }

6eea488597bdb6883afcb2147d1575d179acdd77
RR <RR@MAIN>
2024-06-03 19:14:58 -0400

    s

--- UtilBlazor/Graphics.fs
+++ UtilBlazor/Graphics.fs
@@ -1,6 +1,7 @@
 ﻿module UtilBlazor.Graphics
 
 open System
+open System.Threading.Tasks
 
 open Blazor.Extensions
 open Blazor.Extensions.Canvas
@@ -33,6 +34,56 @@ let essential =
 
     """
 
+type Rect = {
+l: float
+t: float
+w: float
+h: float }
+
+let empty__Rect() = {
+    l = 0.0
+    t = 0.0
+    w = 0.0
+    h = 0.0 }
+
+let palette = 
+    [|  "Red"
+        "Green"
+        "Blue"
+        "Yellow"
+        "Orange"
+        "Purple"
+        "Pink"
+        "Cyan"
+        "Crimson"
+        "MediumVioletRed"
+        "DarkMagenta"
+        "Indigo"
+        "DarkSlateBlue"
+        "MediumBlue"
+        "MidnightBlue"
+        "Navy"
+        "DarkSlateGray"
+        "DarkGreen"
+        "DarkOliveGreen"
+        "SaddleBrown"
+        "Sienna"
+        "Maroon"
+        "Gray"
+        "DimGray"
+        "Black" |]
+
+
+let uint32__color (u:UInt32) = 
+    let s = 
+        u
+        |> BitConverter.GetBytes
+        |> Array.map(fun b -> 
+            b.ToString("X").PadLeft(2,'0'))
+        |> String.Concat
+
+    "#" + s
+
 let rand__color() = 
 
     let r = new Random()
@@ -58,6 +109,25 @@ let drawLine (ctx:Canvas2DContext) (l,t,r,b) =
         do! ctx.LineToAsync(r,b)
         do! ctx.ClosePathAsync()
         do! ctx.StrokeAsync()
+    }
+
+let drawPath (ctx:Canvas2DContext) (points:(float * float)[]) = 
+    task{
+        if points.Length > 0 then
+            do! ctx.BeginPathAsync()
+
+            let mutable p = points[0]
+            do! ctx.MoveToAsync(fst p,snd p)
+            let! tasks = 
+                [| 1 .. points.Length - 1|]
+                |> Array.map(fun i ->
+                    let next = points[i]
+                    let t = ctx.LineToAsync(fst next,snd next)
+                    p <- next
+                    t)
+                |> Task.WhenAll
+            do! ctx.ClosePathAsync()
+            do! ctx.StrokeAsync()
     }
 
 let circle = 2.0 * System.Math.PI    

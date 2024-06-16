4fe6e4c5f3aa221e95bbfed26412441f31d2e33c
R <siduochen@hotmail.com>
2024-06-16 16:24:33 -0400

    s

---
+++ UtilWebServer/Common.fs
@@ -0,0 +1,37 @@
+﻿module UtilWebServer.Common
+
+open System
+open System.Text
+open System.IO
+open System.Diagnostics
+
+open Util.Zmq
+
+let since = DateTime.UtcNow
+
+let output:string -> unit = 
+
+    let assbly = System.Reflection.Assembly.GetCallingAssembly()
+    let dir = Directory.GetCurrentDirectory()
+    if dir.EndsWith "WebService" then
+        Debug.WriteLine
+    else if dir.EndsWith "WebApp" then
+        fun s ->
+            let elapse = DateTime.UtcNow.Subtract since
+            [|
+                elapse.Days.ToString("000") + "."
+                elapse.Hours.ToString("00") + ":"
+                elapse.Minutes.ToString("00") + ":"
+                elapse.Seconds.ToString("00") + "."
+                elapse.Milliseconds.ToString("000") + "> "
+                s |]
+            |> String.Concat
+            |> Debug.WriteLine
+    else
+        Console.OutputEncoding <- Encoding.Unicode
+        Console.WriteLine
+
+let port = 12077
+
+let zweb = create__ZWeb 2 port LogLevel.All false [||]
+

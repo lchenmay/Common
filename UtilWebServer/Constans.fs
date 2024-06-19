15e806e5db81328938480a3530a3d5c48e934857
R <siduochen@hotmail.com>
2024-06-19 16:31:52 -0400

    s

--- UtilWebServer/Constans.fs
+++ UtilWebServer/Constans.fs
@@ -5,6 +5,24 @@ open System.Text
 open System.IO
 open System.Diagnostics
 
+let freqLangCodes = 
+    [|  "en"
+        "zh"
+        "fr"
+        "de"
+        "jp"
+        "it" |]
+
+let freqCurCodes = 
+    [|  "USD"
+        "EUR"
+        "GBP"
+        "CHF"
+        "JPY"
+        "BTC"
+        "ETH"
+        "CNY" |]
+
 let freqBizCodes = 
     [|  "X"
         "GOOGLE"

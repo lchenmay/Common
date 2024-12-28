d43b88df384b1c57c503e3fe7c0bbfa263603b7d
R <siduochen@hotmail.com>
2024-12-28 13:29:11 -0500

    s

---
+++ Util/Media/SixLaborsImageSharp.fs
@@ -0,0 +1,22 @@
+﻿module Util.SixLaborsImageSharp
+
+open System.IO
+
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open SixLabors.ImageSharp.Formats.Jpeg
+
+let generateThumbnail 
+    (w:int,h:int)
+    (input:byte[]) = 
+
+    use image = Image.Load (new MemoryStream(input))
+
+    image.Mutate(fun ctx -> ctx.Resize(w,h) |> ignore)
+
+    let output = new MemoryStream()
+    image.SaveAsPng(output)
+
+    output.ToArray()
+    
+

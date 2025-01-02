6a4126169dcfed41189d639caf2ef3197530ba1e
R <siduochen@hotmail.com>
2025-01-02 10:18:28 -0500

    s

--- UtilWebServer/SSR.fs
+++ UtilWebServer/SSR.fs
@@ -127,3 +127,34 @@ let hHomepage render x =
 let homepage ssr vueDeployDir plugin =
     hHomepage (fun _ -> ssr |> render (vueIndexFile__hashes(vueDeployDir + "/index.html")) plugin)
 
+let hSitemap x__items (x:ReqRep) =
+    if x.req.pathline = "/sitemap.xml" then
+        x.rep <-
+            
+            let w = empty__TextBlockWriter()
+
+            [|  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">"  |]
+            |> w.multiLine
+
+            x__items x
+            |> Array.map(fun (loc) -> 
+            
+                [|  "<url>"
+                    "<loc>" + loc + "</loc>"
+                    //"<lastmod>2025-01-01</lastmod>"
+                    //"<changefreq>daily</changefreq>"
+                    //"<priority>0.8</priority>" 
+                    "</url>"  |]
+                |> String.Concat)
+            |> w.multiLine
+
+            "</urlset>" |> w.newline
+
+            w.text()
+            |> System.Text.Encoding.UTF8.GetBytes
+            |> bin__StandardResponse "text/xml"
+            |> Some
+        Suc x
+    else
+        Fail((),x)

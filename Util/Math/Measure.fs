07b79f89b4a4b367c5b33e5e886116743417b758
R <siduochen@hotmail.com>
2026-07-15 20:48:52 +0800

    s

---
+++ Util/Math/Measure.fs
@@ -0,0 +1,36 @@
+﻿module Util.MathMeasure
+
+open System.Collections.Concurrent
+open System
+open System.Numerics
+
+open Util.Perf
+
+[<Measure>]
+type L
+
+[<Measure>]
+type M
+
+[<Measure>]
+type T
+
+[<Measure>]
+type Pressure = M / (L * T * T)
+
+[<Measure>]
+type psi
+
+[<Measure>]
+type MPa
+
+type PressureUnit = 
+| Psi of float<psi>
+| MPa of float<MPa>
+
+let PressureStandard p =
+    match p with
+    | Psi v -> v * 0.00689476<MPa/psi>
+    | MPa v -> v
+
+
